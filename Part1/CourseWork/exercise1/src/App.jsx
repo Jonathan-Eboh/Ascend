ece4dc21fda3342dd776ab39a20a6f81c7711f6e
Jonathan-Eboh <eboh1923@gmail.com>
2024-01-03 21:48:11 -0500

    parts 1.3 through 1.5 added props to the previous app

--- Part1/CourseWork/exercise1/src/App.jsx
+++ Part1/CourseWork/exercise1/src/App.jsx
@@ -1,94 +1,90 @@
-//Exercises 1.1 and 1.2 hardcoded version on the app
 
-/* 
-
-Unfortunately, the entire application is in the same component. 
-Refactor the code so that it consists of three new components: 
-Header, Content, and Total. 
-All data still resides in the App component, which passes the necessary data to each component using props. 
-
-
-1) Header takes care of rendering the name of the course, --Done
-
-2) Content renders the parts and their number of exercises --Done 
-
-3) and Total renders the total number of exercises. --Done
-
-
-*/
-
+/*
+old Header no prop
 const Header = () => {
   return (
     <div>
-      <h1>Half Stack application development</h1>
+      <h1>Half Stack application development outside</h1>
+    </div>
+  )
+}
+*/
+
+// New Header prop
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-//1.2 
+//new Part with prop
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part}{props.exercises}</p>
+    </div>
+  )
+}
+
+
+
+// new Content with prop
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+
+// new Total with prop
+const Total = (props) => {
+  return (
+    <div>
+      <p>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </div>
+  )
+}
+
 
 /* 
+ Now that we have an object to work with we can now go 
+ back and refactor out compotents with the "skeleton" of how we 
+ eventually want to "unpack" the object at the point or rendering
+ within the App components return statement
 
-1.2: course information, step2
-Refactor the Content component so that it does not render any names of parts or their number of exercises by itself. 
-Instead, it only renders three Part components of which each renders the name and number of exercises of one part.
+ */
 
-*/
-const Part1 = () => {
-  return (
-    <div>
-      <p>Part1: Fundamentals of React. Number of ecercises: 10</p>
-    </div>
-  )
-}
-
-const Part2 = () => {
-  return (
-    <div>
-      <p>Part2: Using props to pass data. Number of ecercises: 7</p>
-    </div>
-  )
-}
-
-const Part3 = () => {
-  return (
-    <div>
-      <p>Part3: State of a component. Number of ecercises: 14</p>
-    </div>
-  )
-}
-const Content = () => {
-  return (
-    <div>
-      <Part1 />
-      <Part2 />
-      <Part3 />
-    </div>
-  )
-}
-
-const Total = () => {
-  return (
-    <div>
-      <p>Number of exercises: {10 + 7 + 14}</p>
-    </div>
-  )
-}
 
 const App = () => {
-  //const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header />
-      <Content />
-      <Total />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
